75512c776a2933d30e8cb757f28fdfe2836f605d
Travis Beatty <travisby@gmail.com>
2011-07-31 21:43:21 -0400

    choose a variation of 2of4, and distribute roles randomly to 9 players

--- init.py
+++ init.py
@@ -1,75 +1,79 @@
 import sqlite3
 import os
 from main import main
-def newdb(dbname):
-	db = sqlite3.connect(dbname)
-	db.executescript('''
-			CREATE TABLE PLAYERS (
-                        id SMALLINT PRIMARY KEY,
-                        name VARCHAR,
-                        password CHARACTER(60),
-                        status SMALLINT,
-                        role SMALLINT,
-                        known VARCHAR);
-			
-			CREATE TABLE ADMIN (
-			status SMALLINT DEFAULT "0");
 
-			INSERT INTO ADMIN (status) values ("0");
-		''')
-	db.commit()
-	db.close()
-	main(dbname)
+
+def newdb(dbname):
+    db = sqlite3.connect(dbname)
+    db.executescript('''
+        CREATE TABLE PLAYERS (
+        id SMALLINT PRIMARY KEY,
+        name VARCHAR,
+        password CHARACTER(60),
+        status SMALLINT,
+        role VARCHAR,
+        known VARCHAR);
+
+        CREATE TABLE ADMIN (
+        status SMALLINT
+        gtime SMALLINT );
+
+        INSERT INTO ADMIN (status) values ("0");
+        ''')
+    db.commit()
+    db.close()
+    main(dbname)
 
 def testifdb(dbname):
-	db = sqlite3.connect(dbname)    
-	try: 
-		db.execute('CREATE TABLE TEST (a INTEGER);')
-	except sqlite3.DatabaseError:
-		db.close()
-		return 1
-	db.execute('DROP TABLE TEST;')
-	db.close()
-	return 0
+    db = sqlite3.connect(dbname)
+    try:
+        db.execute('CREATE TABLE TEST (a INTEGER);')
+    except sqlite3.DatabaseError:
+        db.close()
+        return 1
+    db.execute('DROP TABLE TEST;')
+    db.close()
+    return 0
 
-def init(): 
-	print '''
-	
-	
-	                                             ffffffffffffffff    iiii                    
-	                                            f::::::::::::::::f  i::::i                   
-	                                           f::::::::::::::::::f  iiii                    
-	                                           f::::::fffffff:::::f                          
-	   mmmmmmm    mmmmmmm     aaaaaaaaaaaaa    f:::::f       ffffffiiiiiii   aaaaaaaaaaaaa   
-	 mm:::::::m  m:::::::mm   a::::::::::::a   f:::::f             i:::::i   a::::::::::::a  
-	m::::::::::mm::::::::::m  aaaaaaaaa:::::a f:::::::ffffff        i::::i   aaaaaaaaa:::::a 
-	m::::::::::::::::::::::m           a::::a f::::::::::::f        i::::i            a::::a 
-	m:::::mmm::::::mmm:::::m    aaaaaaa:::::a f::::::::::::f        i::::i     aaaaaaa:::::a 
-	m::::m   m::::m   m::::m  aa::::::::::::a f:::::::ffffff        i::::i   aa::::::::::::a 
-	m::::m   m::::m   m::::m a::::aaaa::::::a  f:::::f              i::::i  a::::aaaa::::::a 
-	m::::m   m::::m   m::::ma::::a    a:::::a  f:::::f              i::::i a::::a    a:::::a 
-	m::::m   m::::m   m::::ma::::a    a:::::a f:::::::f            i::::::ia::::a    a:::::a 
-	m::::m   m::::m   m::::ma:::::aaaa::::::a f:::::::f            i::::::ia:::::aaaa::::::a 
-	m::::m   m::::m   m::::m a::::::::::aa:::af:::::::f            i::::::i a::::::::::aa:::a
-	mmmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaafffffffff            iiiiiiii  aaaaaaaaaa  aaaa
-	
-	
-	by Travis Beatty (travisby@gmail.com)
-	
-	
-	'''
-	
-	dbname = raw_input("Please enter the game name you wish to connect to, or the name of a new game: ")
-	
-	if os.path.isfile(dbname) == True:
-		if  testifdb(dbname) == 1:
-			print "The filename you provided is not an sqlite database.  Please try again, or contact an administrator."
-			init()
-		main(dbname)
-		
-	else:
-		if raw_input("Are you sure you wish to create a new game? (y/n)") == "y":
-			newdb(dbname)
-		else:
-			init()
+def init():
+    print '''
+
+
+                                                 ffffffffffffffff    iiii
+                                                f::::::::::::::::f  i::::i
+                                               f::::::::::::::::::f  iiii
+                                               f::::::fffffff:::::f
+       mmmmmmm    mmmmmmm     aaaaaaaaaaaaa    f:::::f       ffffffiiiiiii   aaaaaaaaaaaaa
+     mm:::::::m  m:::::::mm   a::::::::::::a   f:::::f             i:::::i   a::::::::::::a
+    m::::::::::mm::::::::::m  aaaaaaaaa:::::a f:::::::ffffff        i::::i   aaaaaaaaa:::::a
+    m::::::::::::::::::::::m           a::::a f::::::::::::f        i::::i            a::::a
+    m:::::mmm::::::mmm:::::m    aaaaaaa:::::a f::::::::::::f        i::::i     aaaaaaa:::::a
+    m::::m   m::::m   m::::m  aa::::::::::::a f:::::::ffffff        i::::i   aa::::::::::::a
+    m::::m   m::::m   m::::m a::::aaaa::::::a  f:::::f              i::::i  a::::aaaa::::::a
+    m::::m   m::::m   m::::ma::::a    a:::::a  f:::::f              i::::i a::::a    a:::::a
+    m::::m   m::::m   m::::ma::::a    a:::::a f:::::::f            i::::::ia::::a    a:::::a
+    m::::m   m::::m   m::::ma:::::aaaa::::::a f:::::::f            i::::::ia:::::aaaa::::::a
+    m::::m   m::::m   m::::m a::::::::::aa:::af:::::::f            i::::::i a::::::::::aa:::a
+    mmmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaafffffffff            iiiiiiii  aaaaaaaaaa  aaaa
+
+
+    by Travis Beatty (travisby@gmail.com)
+
+
+    '''
+
+    dbname = raw_input("Please enter the game name you wish to connect to, or the name of a new game: ")
+
+    if os.path.isfile(dbname) == True:
+        if  testifdb(dbname) == 1:
+            print "The filename you provided is not an sqlite database.  Please try again, or contact an administrator."
+            init()
+        else:
+            main(dbname)
+
+    else:
+        if raw_input("Are you sure you wish to create a new game? (y/n)") == "y":
+            newdb(dbname)
+        else:
+            init()
 init()
--- main.py
+++ main.py
@@ -1,55 +1,94 @@
 import sqlite3
 import bcrypt
-import getpass 
+import getpass
+import random
+
+
+def getvariation():
+    x = random.randint(0,5)
+    if x == 0: return ["goon","mcop","townie","townie","townie","townie","townie","cop","doctor"]
+    elif x == 1: return ["goon","mcop","townie","townie","townie","townie","townie","cop","jailkeeper"]
+    elif x == 2: return ["goon","mcop","townie","townie","townie","townie","townie","doctor","jailkeeper"]
+    elif x == 3: return ["goon","mcop","townie","townie","townie","townie","townie","townie","cop"]
+    elif x == 4: return ["goon","mcop","townie","townie","townie","townie","townie","townie","jailkeeper"]
+    elif x == 5: return ["goon","mcop","townie","townie","townie","townie","townie","townie","doctor"]
+
+
+def distribroles(dbname):
+    db = sqlite3.connect(dbname)
+    db_cur = db.cursor()
+    roles = getvariation()
+    random.shuffle(roles) # Note, random.shuffle() does not return anything.  It modifies the list.
+    db_cur.execute('SELECT name FROM PLAYERS;')
+    players = db_cur.fetchall()
+    x = 0
+    for i in range(len(players)):
+        db_cur.execute('UPDATE PLAYERS SET role=? WHERE name=?;',[roles[i],players[i][0]])
+	db.commit()
+    db.close()
+
+
+def displayopts(dbname,user):
+    db = sqlite3.connect(dbname)
+    db_cur = db.cursor()
+    db_cur.execute('SELECT role FROM PLAYERS WHERE name=?;',[user])
+    db.close()
+
+
 def mainscreen(dbname,user):
-	db = sqlite3.connect(dbname)
-	db_cur = db.cursor()
-#	print "Phase: " + db_cur.execute('SELECT gtime FROM ADMIN;')
-	db_cur.execute('SELECT name FROM PLAYERS WHERE status=1;')
-	print "Alive Players: ",db_cur.fetchall()
-	print "Dead players: " #+ db_cur.execute(SELECT name FROM PLAYERS WHERE status=0;)
-	db.close()	
+    db = sqlite3.connect(dbname)
+    db_cur = db.cursor()
+    print "Phase: " + db_cur.execute('SELECT gtime FROM ADMIN;')
+    db_cur.execute('SELECT name FROM PLAYERS WHERE status=1;')
+    print "Alive Players: ", db_cur.fetchall()
+    db_cur.execute('SELECT name FROM PLAYERS WHERE status=0;')
+    print "Dead players: ", db_cur.fetchall()
+    db.close()
+    displayopts(dbname,user)
+
+
 def register(dbname):
-	user = raw_input("Please enter your username: ")
-	pw = getpass.getpass("Please enter your password: ")
-	if pw == getpass.getpass("Please enter your password again: "):
-		hashed = bcrypt.hashpw(pw,bcrypt.gensalt(12))		
-		db = sqlite3.connect(dbname)
-		db.execute('INSERT INTO PLAYERS (name, password) values (?, ?)', [user,hashed])
-		db.commit()
-		db.close()
-	else:
-		register(dbname)
+    user = raw_input("Please enter your username: ")
+    pw = getpass.getpass("Please enter your password: ")
+    if pw == getpass.getpass("Please enter your password again: "):
+        hashed = bcrypt.hashpw(pw,bcrypt.gensalt(12))
+        db = sqlite3.connect(dbname)
+        db.execute('INSERT INTO PLAYERS (name, password) values (?, ?)', [user,hashed])
+        db.commit()
+        db.close()
+    else:
+        register(dbname)
+
 
 def main(dbname):
-	db = sqlite3.connect(dbname)
-	db_cur = db.cursor()
-	db_cur.execute('SELECT status FROM ADMIN;')
-	gstatus = db_cur.fetchone()[0]
-	db.close()
-	if  gstatus  == 0:
-		raw_input("Please press enter to register for the new game")
-		register(dbname)
-	else:
-		login(dbname)
- 	db.close()
+    db = sqlite3.connect(dbname)
+    db_cur = db.cursor()
+    db_cur.execute('SELECT status FROM ADMIN;')
+    gstatus = db_cur.fetchone()[0]
+    db.close()
+    if  gstatus == 0:
+        raw_input("Please press enter to register for the new game")
+        register(dbname)
+    else:
+        login(dbname)
+
 
 def login(dbname):
-	user = raw_input("Username: ")
-	pw = getpass.getpass("Password: ")
-	db = sqlite3.connect(dbname)
-	db_cur = db.cursor()
-	db_cur.execute("SELECT password FROM PLAYERS WHERE name = ?;", [user])
-	try:
-		dbhashed = db_cur.fetchone()[0]
-	except TypeError:
-		db.close()
-		print "Incorrect username/password, please try again"
-		login(dbname)
-	db.close()
-	if bcrypt.hashpw(pw, dbhashed) == dbhashed:
-		print "Login successful!"
-		mainscreen(dbname,user)
-	else:
-		print "Incorrect username/password, please try again"
-		login(dbname)
+    user = raw_input("Username: ")
+    pw = getpass.getpass("Password: ")
+    db = sqlite3.connect(dbname)
+    db_cur = db.cursor()
+    db_cur.execute("SELECT password FROM PLAYERS WHERE name = ?;", [user])
+    try:
+        dbhashed = db_cur.fetchone()[0]
+    except TypeError:
+        db.close()
+        print "Incorrect username/password, please try again"
+        login(dbname)
+    db.close()
+    if bcrypt.hashpw(pw, dbhashed) == dbhashed:
+        print "Login successful!"
+        mainscreen(dbname,user)
+    else:
+        print "Incorrect username/password, please try again"
+        login(dbname)
