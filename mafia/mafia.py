ab175109673b1c805ecae60850a682bb872e49a9
Travis Beatty <travisby@gmail.com>
2012-02-17 22:35:47 -0500

    Push with new structure

---
+++ mafia/mafia.py
@@ -0,0 +1,8 @@
+'''
+Created on Feb 17, 2012
+
+@author: travis
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ mafia/user.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 17, 2012
+
+@author: travis
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
