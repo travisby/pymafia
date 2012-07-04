ac980a6f1cb1212161da259137005c489d2a01d5
Travis Beatty <travisby@gmail.com>
2012-07-04 12:13:31 -0400

    Added a fixture to load the default skills.  Removed an unneeded field from the skill model.  Fixed #13

--- models/skill.py
+++ models/skill.py
@@ -3,7 +3,6 @@ from django.db import models
 class Skill(models.Model):
     """Skill Model"""
     name = models.CharField(max_length=8, help_text="Skill's displaying name")
-    ability = models.PositiveSmallIntegerField(help_text='Defined ability')
 
     class Meta:
         app_label = 'pymafia'
