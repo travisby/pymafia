8cf87806a3c367ba4bfe06a4471e57f08c252f88
Travis Beatty <travisby@gmail.com>
2012-12-01 11:42:32 -0500

    Updated action model to include TWO players, an offending and defending.

--- models/action.py
+++ models/action.py
@@ -7,8 +7,9 @@ class Action(models.Model):
     """Records all actions (voting, kills, etc), by game and characters"""
 
     time = models.PositiveSmallIntegerField(help_text='Recorded time of action')
-    player = models.ForeignKey(Player, help_text='Peforming Character')
+    performing_player = models.ForeignKey(Player, help_text='Peforming Character', related_name='performing_player')
     skill = models.ForeignKey(Skill, help_text='Used skill')
+    performed_against_player = models.ForeignKey(Player, help_text='Player that the skill was used on', related_name='performed_against_player')
 
 #    def __unicode__(self):
 #        return (self.player + self.skill)
