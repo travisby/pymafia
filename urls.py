4c673a087c574768bd244ee4e0209dec3c05d2dd
Travis Beatty <travisby@gmail.com>
2012-12-01 13:17:07 -0500

    Added new resuorces to the URLs

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@
 
 from django.conf.urls.defaults import *
 from tastypie.api import Api
-from pymafia.api import GameResource, ActionResource, PlayerResource, ClassificationResource
+from pymafia.api import GameResource, ActionResource, PlayerResource, ClassificationResource, AlignmentResource, SkillResource
 
 v1_api = Api()
 
@@ -10,6 +10,8 @@ v1_api.register(GameResource())
 v1_api.register(ActionResource())
 v1_api.register(PlayerResource())
 v1_api.register(ClassificationResource())
+v1_api.register(AlignmentResource())
+v1_api.register(SkillResource())
 
 urlpatterns = patterns('',
     (r'', include(v1_api.urls)),
