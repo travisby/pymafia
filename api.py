39e1ac425ba121a65b2f8b510bcfabd8d4bdf834
Travis Beatty <travisby@gmail.com>
2012-12-01 13:07:08 -0500

    Added every class to the API

--- api.py
+++ api.py
@@ -1,20 +1,33 @@
-from tastypie.resources import ModelResource, Resource, ALL
+from tastypie.resources import ModelResource
 
-from pymafia.models import *
+from pymafia.models import Game, Action, Player, Classification, Alignment, Skill
 
 
 class GameResource(ModelResource):
     class Meta:
         queryset = Game.objects.all()
 
+
 class ActionResource(ModelResource):
     class Meta:
         queryset = Action.objects.all()
+
 
 class PlayerResource(ModelResource):
     class Meta:
         queryset = Player.objects.all()
 
+
 class ClassificationResource(ModelResource):
     class Meta:
         queryset = Classification.objects.all()
+
+
+class AlignmentResource(ModelResource):
+    class Meta:
+        queryset = Alignment.objects.all()
+
+
+class SkillResource(ModelResource):
+    class Meta:
+        queryset = Skill.objects.all()
