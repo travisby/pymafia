2a76cddc1c5a48a06043cf6cf98967925b421065
Travis Beatty <travisby@gmail.com>
2012-12-01 13:50:25 -0500

    Limited HTTP methods on api

--- api.py
+++ api.py
@@ -6,28 +6,35 @@ from pymafia.models import Game, Action, Player, Classification, Alignment, Skil
 class GameResource(ModelResource):
     class Meta:
         queryset = Game.objects.all()
+        allowed_methods = ('get', 'post')
 
 
 class ActionResource(ModelResource):
     class Meta:
         queryset = Action.objects.all()
+        allowed_methods = ('get', 'post')
 
 
 class PlayerResource(ModelResource):
     class Meta:
         queryset = Player.objects.all()
+        fields = ('name', 'alive', 'game')
+        allowed_methods = ('get',)
 
 
 class ClassificationResource(ModelResource):
     class Meta:
         queryset = Classification.objects.all()
+        allowed_methods = ('get', 'post')
 
 
 class AlignmentResource(ModelResource):
     class Meta:
         queryset = Alignment.objects.all()
+        allowed_methods = ('get',)
 
 
 class SkillResource(ModelResource):
     class Meta:
         queryset = Skill.objects.all()
+        allowed_methods = ('get',)
