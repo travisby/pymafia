0a4fe7972524885e12af42f134c10d627df5fe27
Travis Beatty <travisby@gmail.com>
2012-12-06 12:25:07 -0500

    Fixed user api

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ from pymafia.models import Game, Action, Player, Classification, Alignment, Skil
 
 class UserResource(ModelResource):
 
-    players = fields.ToManyField('pymafia.api.PlayerResource', attribute = lambda x : x.obj.player_set.all())
+    players = fields.ToManyField('pymafia.api.PlayerResource', 'player_set')
 
     class Meta:
         queryset = User.objects.all()
